0d26d6a5d52fa3731f528db077afa776e54cb506
bhargavgajare1479 <gajarebhargav@gmail.com>
2026-03-07 08:44:57 +0530

    Initialise Unearth showcase website

---
+++ src/components/LenisProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function LenisProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true,
+      lerp: 0.08,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
